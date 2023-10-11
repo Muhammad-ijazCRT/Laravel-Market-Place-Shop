21a40b45ad874f35da7b9286841a77fd70a2fa0e
Muhammad ijaz <75518471+Muhammad-ijazCRT@users.noreply.github.com>
2023-10-11 13:43:16 +0500

    doen

--- app/Http/Controllers/Payment/Checkout/ManualPaymentController.php
+++ app/Http/Controllers/Payment/Checkout/ManualPaymentController.php
@@ -150,7 +150,6 @@ class ManualPaymentController extends CheckoutBaseControlller
 
         }
 
-        return $input;
         $data = $order->fill($input)->save();
         $order->tracks()->create(['title' => 'Pending', 'text' => 'You have successfully placed your order.' ]);
         $order->notifications()->create();
