dee16162c2f38a94e2411a0035feb4eefc5c7599
Muhammad ijaz <75518471+Muhammad-ijazCRT@users.noreply.github.com>
2023-10-12 09:47:29 +0500

    update

--- app/Http/Controllers/Payment/Checkout/ManualPaymentController.php
+++ app/Http/Controllers/Payment/Checkout/ManualPaymentController.php
@@ -55,28 +55,37 @@ class ManualPaymentController extends CheckoutBaseControlller
         }
 
 
+        // dd('asfdasd');
         
 
         $mlm_wallet = isset($response['user']['wallet']) ? (int)$response['user']['wallet'] : 0;
         $mlm_balance = isset($response['user']['balance']) ? (int)$response['user']['balance'] : 0;
 
         $mlm_total = $mlm_wallet + $mlm_balance;
-
-        // $request->total =200;
-        if ($mlm_wallet > $request->total) {
-            $mlm_wallet = $mlm_wallet - $request->total;
-        } elseif ($mlm_wallet < $request->total && $mlm_total > $request->total) {
-            $need_from_balance = $request->total - $mlm_wallet;
-            $mlm_balance -= $need_from_balance;
-            $mlm_wallet = 0;
-        }else{
+        if($mlm_total < $request->total)
+        {
             return redirect()->back()->with('unsuccess','you have insufficient amount!');
             // return redirect()->back();
         }
+
+        $request->total =200;
+        // if ($mlm_wallet > $request->total) {
+        //     $mlm_wallet = $mlm_wallet - $request->total;
+        // } elseif ($mlm_wallet < $request->total && $mlm_total > $request->total) {
+        //     $need_from_balance = $request->total - $mlm_wallet;
+        //     $mlm_balance -= $need_from_balance;
+        //     $mlm_wallet = 0;
+        // }else{
+        //     return redirect()->back()->with('unsuccess','you have insufficient amount!');
+        //     // return redirect()->back();
+        // }
         
 
         // dd($request->total);
         // dd($mlm_balance, $mlm_wallet);
+
+
+
 
         
 
@@ -173,30 +182,26 @@ class ManualPaymentController extends CheckoutBaseControlller
             }
         }
 
-        OrderHelper::size_qty_check($cart); // For Size Quantiy Checking
-        OrderHelper::stock_check($cart); // For Stock Checking
-        OrderHelper::vendor_order_check($cart,$order); // For Vendor Order Checking
+        // OrderHelper::size_qty_check($cart); // For Size Quantiy Checking
+        // OrderHelper::stock_check($cart); // For Stock Checking
+        // OrderHelper::vendor_order_check($cart,$order); // For Vendor Order Checking
         
-        Session::put('temporder',$order);
-        Session::put('tempcart',$cart);
-        Session::forget('cart');
-        Session::forget('already');
-        Session::forget('coupon');
-        Session::forget('coupon_total');
-        Session::forget('coupon_total1');
-        Session::forget('coupon_percentage');
+        // Session::put('temporder',$order);
+        // Session::put('tempcart',$cart);
+        // Session::forget('cart');
+        // Session::forget('already');
+        // Session::forget('coupon');
+        // Session::forget('coupon_total');
+        // Session::forget('coupon_total1');
+        // Session::forget('coupon_percentage');
+
+
 
         if ($order->user_id != 0 && $order->wallet_price != 0) {
             OrderHelper::add_to_transaction($order,$order->wallet_price); // Store To Transactions
         }
 
 
-        $data = [
-            "email"=> $request->mlm_email,
-            "password"=> $request->mlm_password,
-            "mlm_wallet"=> $mlm_wallet,
-            "mlm_balance"=> $mlm_balance
-        ];
 
 
         // adding price to seller account
@@ -225,12 +230,20 @@ class ManualPaymentController extends CheckoutBaseControlller
             }
         }
 
+       
+        
+        $data = [
+            "email"=> $request->mlm_email,
+            "password"=> $request->mlm_password,
+            "product_title"=> 'product sadfa',
+            'product_price' => $request->total
+        ];
 
+
+        $response = Http::post('https://staging.kosmomoney.com/api/update-user-amount', $data);
+        // $response = Http::post('http://localhost:8001/api/update-user-amount', $data);
         
 
-        // $response = Http::post('http://127.0.0.1:8001/api/update-user-amount', $data);
-        // $response = Http::post('https://staging.kosmomoney.com/api/update-user-amount', $data);
-        // return $response;
 
         //Sending Email To Buyer
         // $data = [
@@ -257,6 +270,6 @@ class ManualPaymentController extends CheckoutBaseControlller
         // $mailer->sendCustomMail($data);
         
 
-        return redirect($success_url)->with('success',__("Thanks for purchasing the product."));
+        return redirect($success_url)->with('success',"Thanks for purchasing the product.");
     }
 }
